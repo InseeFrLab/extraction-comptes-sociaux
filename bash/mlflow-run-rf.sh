eb69d99780f2e34146c0cb75cf22feb505b39313
tomseimandi <tom.seimandi@gmail.com>
2023-01-31 11:51:27 +0100

    Fix run path in bash scripts

--- bash/mlflow-run-rf.sh
+++ bash/mlflow-run-rf.sh
@@ -16,4 +16,4 @@ done <<< "$GET_PODS"
 export MLFLOW_TRACKING_URI="https://projet-extraction-tableaux-$POD_ID.user.lab.sspcloud.fr"
 export MLFLOW_EXPERIMENT_NAME="page_selection"
 
-mlflow run ~/work/comptes-sociaux-to-tableau-fp-csv/ --entry-point page_selection --env-manager=local -P remote_server_uri=$MLFLOW_TRACKING_URI 
+mlflow run ~/work/extraction-comptes-sociaux/ --entry-point page_selection --env-manager=local -P remote_server_uri=$MLFLOW_TRACKING_URI
--- bash/mlflow-run-tablenet.sh
+++ bash/mlflow-run-tablenet.sh
@@ -16,4 +16,4 @@ done <<< "$GET_PODS"
 export MLFLOW_TRACKING_URI="https://projet-extraction-tableaux-$POD_ID.user.lab.sspcloud.fr"
 export MLFLOW_EXPERIMENT_NAME="extraction"
 
-mlflow run ~/work/comptes-sociaux-to-tableau-fp-csv/ --entry-point extraction --env-manager=local -P remote_server_uri=$MLFLOW_TRACKING_URI -P config_path=config/tablenet_config/tablenet_config_cycle.yaml
+mlflow run ~/work/extraction-comptes-sociaux/ --entry-point extraction --env-manager=local -P remote_server_uri=$MLFLOW_TRACKING_URI -P config_path=config/tablenet_config/tablenet_config.yaml
