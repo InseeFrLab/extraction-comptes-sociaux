ed373cedfbebad23a70eec2dd985be2f0d796fe3
tomseimandi <tom.seimandi@gmail.com>
2023-01-31 09:28:47 +0000

    Add pre-commit setup

--- setup.sh
+++ setup.sh
@@ -13,6 +13,9 @@ sudo apt -y install ghostscript python3-tk
 # Installing Python requirements
 pip install -r requirements.txt
 
+# Install pre-commit
+pre-commit install
+
 # Importing data
 export MC_HOST_minio=https://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY:$AWS_SESSION_TOKEN@$AWS_S3_ENDPOINT
 mc cp -r minio/projet-extraction-tableaux/data/marmot_data data/
