d185781ecead508e1941bdfdd5570ca3627f8689
tseimandi <tom.seimandi@insee.fr>
2023-02-02 18:51:52 +0100

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@
 # Installing packages
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get -yq install software-properties-common
+sudo apt install software-properties-common
+sudo apt-get update
 sudo add-apt-repository ppa:alex-p/tesseract-ocr-devel
 sudo apt -y install python3-opencv
 sudo apt -y install tesseract-ocr
