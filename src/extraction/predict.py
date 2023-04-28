860d24e8448293dbc5a827e949ade2e176aee969
tseimandi <tom.seimandi@insee.fr>
2023-04-28 15:21:08 +0200

    Fix predict and marmot dataset

--- src/extraction/predict.py
+++ src/extraction/predict.py
@@ -6,40 +6,25 @@ from pathlib import Path
 from PIL import Image
 from matplotlib import pyplot as plt
 from argparse import ArgumentParser
+import mlflow
+import torch
 
 from .data import fs
 from .table_extractor import TableExtractor
+import numpy as np
 
 
 def main(args):
     """
     Main method.
     """
-    # Transformations applied to test data.
-    transforms = album.Compose(
-        [
-            album.Resize(896, 896, always_apply=True),
-            album.Normalize(),
-            ToTensorV2(),
-        ]
+    model_name = "extraction"
+    version = args.version
+    clf = mlflow.pytorch.load_model(
+        f"models:/{model_name}/{version}", map_location=torch.device("cpu")
     )
 
-    checkpoint_path = (
-        "projet-extraction-tableaux/logs/TableNetModule/"
-        + "version_"
-        + "00"
-        + "/checkpoints/"
-    )
-    table_extractor = None
-    for checkpoints in fs.ls(checkpoint_path):
-        if (
-            Path(checkpoints).suffix == ".ckpt"
-            and Path(checkpoints).name != "last.ckpt"
-        ):
-            table_extractor = TableExtractor(
-                checkpoint_path=checkpoints, transforms=transforms
-            )
-            break
+    table_extractor = TableExtractor(model=clf)
 
     TEST_DATA = [
         "305756413",
@@ -58,7 +43,6 @@ def main(args):
         "702012956",
         "797080850",
     ]
-    TEST_DATA = ["301940219"]
     data_dir = "./data/marmot_data"
     test_images = [
         Path(data_dir).joinpath(path + ".bmp") for path in TEST_DATA
@@ -72,27 +56,17 @@ def main(args):
         plt.plot()
         plt.imshow(out["table_mask"], interpolation="none")
         plt.colorbar(orientation="vertical")
-        plt.savefig("table_mask.png")
+        plt.savefig(siren + "_table_mask.png")
 
         plt.rcParams["figure.figsize"] = (20, 10)
         plt.plot()
         plt.imshow(out["column_mask"], interpolation="none")
         plt.colorbar(orientation="vertical")
-        plt.savefig("column_mask.png")
+        plt.savefig(siren + "_column_mask.png")
 
         for i, df in enumerate(out["tables"]):
-            save_path = (
-                "projet-extraction-tableaux/logs/TableNetModule/"
-                + "version_"
-                + str(args.version)
-                + "/test_output/"
-                + siren
-                + "_table_"
-                + str(i)
-                + ".csv"
-            )
-            with fs.open(save_path, "w") as f:
-                df.to_csv(f, index=False, index_label=False)
+            save_path = siren + "_table_" + str(i) + ".csv"
+            df.to_csv(save_path, index=False, index_label=False)
 
 
 if __name__ == "__main__":
